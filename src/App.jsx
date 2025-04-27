b23509d88f71bc09589973f9d6755f591d205adb
Dishanth Ivan <ivandishanth@gmail.com>
2025-04-27 17:54:01 +0530

    3rdcommit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   
       <Routes>
         <Route path="/" element={<Home />} />
-      
         <Route path="/contact" element={<Contact />} />
         <Route path="/reactwork4" element={<Home />} />
       </Routes>
